3ab1380b7c278cd9041b15980dc711dbbd7033d7
kazemicode <sarieses@gmail.com>
2018-11-26 18:56:54 -0800

    Added comments

--- Lab12.py
+++ Lab12.py
@@ -1,22 +1,12 @@
 # Team SCSI Logic
 # Sara Kazemi, Cody Young, Nathan Warren-Acord, Ryan Dorrity
-# Lab 11
-# 11/22/2018
+# Lab 12
+# 11/26/2018
 # ###########################################################
 
-# Welcome everyone! This is just a template I started for the lab.
-# Please feel free to change anything. I thought this might help 
-# keep us organized, but please share any ideas you have for 
-# this lab.
-
-# Please note that any code you see in my section is mostly just
-# for my own practice and not meant as a basis for how we should 
-# code this project together.
+# Welcome to Stargate: SCSI-1! a text-based adventure
 
 
-##############################################################
-##############################################################
-#############          Sara Kazemi        ####################
 # Game map:
 #
 #                     --------------------
@@ -51,9 +41,16 @@
 
 import re
 
-
+# The Location class is used to initialize all of our rooms
+# Each Location has:
+# String: name (prints when you visit a room)
+# String: description (prints the first time you visit a room or of you "look" at a room).
+# List: interactions (items in the room you can interact with)
+# Dictionary: connections (keys are Strings that refer to a valid direction a character can move from this Location
+# and the values are reference variables of the rooms corresponding to those directions.
+# boolean: visited: Initialized to False and changes to True once the player moves to a new room.
 class Location:
-
+    # Constructor for our locations.
     def __init__(self, name, description, interactions, connections):
         self.name = name
         self.description = description
@@ -63,18 +60,23 @@ class Location:
 
     def print_description(self):
         print("\n\t~~" + self.name + "~~\n")
-        if not self.visited:
+        if not self.visited:    # Only print the description of the room if Player hasn't been there before
+                                # Note: Player can use "look" to see the description again
             print(self.description)
 
     def remove_item(self, item):
-        if item in itemTable:
+        if item in itemTable:   # When an item is removed from the room, we should remove its description
+                                # from our itemTable and replace it with an empty string.
+                                # This removes the item description from the room's description.
             self.description = self.description.replace(itemTable[item][2], "")
 
-    def __getitem__(self):
+    def __getitem__(self):      # helper function. Not really used at the moment. Remove?
         return self.name
 
 
 # Room Definitions
+# Rooms are created ahead of time with dummy empty values because we need to refer to these objects in their
+# connections dictionary.
 main_room = Location("", "", [], {})
 north_room = Location("", "", [], {})
 south_room = Location("", "", [], {})
@@ -82,12 +84,18 @@ east_room = Location("", "", [], {})
 west_room = Location("", "", [], {})
 
 # Items in the world
+# A dictionary that holds all the items in the world as keys. The values are lists.
+# The 0th index of that list holds the description of the item when the Player examines the item
+# The 1st index of the list holds the reference object (a Location or Player) that possesses the item
+# The 2nd index of the list holds the description of the item that gets concatenated to the room's location
+# if it is in that room. This description turns into the empty string when a Player takes the item.
 itemTable = {
     "piece of metal": ["\nRadiates with a strange glow. Smooth to the touch.", main_room, "\nA shiny piece of\
  metal catches your eye on the west wall."]
 }
 
 # Room Initializations
+# Now we can assign the instance variables with our real values without breaking the code.
 
 # Main Room
 main_room.name = "Main Room"
@@ -133,6 +141,14 @@ glass. There is a door to the east."
 west_room.interactions = []
 west_room.connections = {"east": main_room}
 
+
+
+
+
+# Handing user input
+# We use regular expressions to find matches for valid commands
+# These are called in our user_input function to determine
+# What the appropriate response should be
 cmdMove = re.compile(("^(north|n|south|s|west|w|east|e|up|down){1}$"), re.I)
 cmdExit = re.compile(("^(Quit|Exit){1}$"), re.I)
 cmdInv = re.compile("^(Inventory){1}$", re.I)
@@ -142,20 +158,21 @@ cmdExamine = re.compile(("^Examine\s((\w+)(?:\s)?){1,4}$"), re.I)
 cmdTake = re.compile(("^Take\s((\w+)(?:\s)?){1,4}$"), re.I)
 cmdDrop = re.compile(("^Drop\s((\w+)(?:\s)?){1,4}$"), re.I)
 
-
+# user_input takes a String cmmd and determines if it matches any of our regular expressions
+# and responds accordingly
 def user_input(cmmd):
-    if cmdExit.search(cmmd):
+    if cmdExit.search(cmmd):                        # exits game if Player inputs "exit" or "quit"
         print "Game Over. Thanks for playing!"
         raise SystemExit
-    elif cmdHelp.search(cmmd):
+    elif cmdHelp.search(cmmd):                      # re-prints out the directions if the Player inputs "help"
         print_directions()
-    elif cmdInv.search(cmmd):
-        p.print_inventory()
-    elif cmdLook.search(cmmd):
-        print p.location.description
-    elif cmdExamine.search(cmmd):
+    elif cmdInv.search(cmmd):                       # prints out the contents of the Player's inventory
+        p.print_inventory()                         # to do: Print out "nothing in inventory" if empty
+    elif cmdLook.search(cmmd):                      # prints out the description of the Player's Location
+        print p.location.description                # when Player inputs "look"
+    elif cmdExamine.search(cmmd):                   # prints out item's description if the Player types examine
         examine = cmdExamine.search(cmmd)
-        examine = re.sub("examine ", "", examine.group(), 1)
+        examine = re.sub("examine ", "", examine.group(), 1) # need to fix
         # Examine function call would go here <--
         # if examine in (player inv) or examine in (room inv):
         # Print out that object description
@@ -207,20 +224,23 @@ class Player():
                 print item
 
 
-def print_welcome():
+def print_intro():
     print "Your body aches. There is flowing water, somewhere, but you cannot tell where.\n\
 Rolling over, blades of grass tickle your skin and the smell of ash brings\n\
 burning tears. As you wipe your eyes, the surrounding structure comes into focus.\n\
 You look around and discover you are in the ... "
 
+def print_welcome():
+    print "\t\t---------------------------------------------------------------\n"
+    print "\t\t\t\t\t\tWelcome to Stargate: SCSI-1!\n\n\
+    \tTo navigate the game world, type the following commands.\n\
+    \tAlternate commands are followed with a slash (e.g. command1/command2).\n\
+    \tCommands are not case sensitive.\n"
+    print "\t\t---------------------------------------------------------------"
 
 def print_directions():
-    print "\t\t---------------------------------------------------------------\n"
-    print "\t\t\t\tWelcome to Stargate: SCSI-1!\n\t\tTo navigate the game world, type the following commands.\n\
-\t\tAlternate commands are followed with a slash (e.g. command1/command2).\n\
-\t\tCommands are not case sensitive.\n"
-    print "\t\t---------------------------------------------------------------"
-    print "\n\n_Movement_\n\
+
+ print "\n\n _Movement_\n\
  Move north: n/north\n\
  Move south: s/south\n\
  Move west: w/west\n\
@@ -236,9 +256,10 @@ To access this help menu at any time, type \"help\".\n"
 
 
 def main():
+    print_welcome()
     print_directions()
     raw_input("Press Enter to continue...\n")
-    print_welcome()
+    print_intro()
     Location.print_description(main_room)
     # run until exit or player dies
     while True:
